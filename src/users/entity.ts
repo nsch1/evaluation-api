2060638779dc53db7e90a2bf800ab6e16402e8dc
nsch1 <nick.schooneman1@gmail.com>
2018-04-03 12:26:47 +0200

    Add user entity

---
+++ src/users/entity.ts
@@ -0,0 +1,25 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm"
+import * as bcrypt from 'bcrypt'
+
+@Entity()
+export default class User {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('text', {unique: true})
+  email: string
+
+  @Column('text')
+  password: string
+
+  async setPassword(pass: string) {
+    const hash = await bcrypt.hash(pass, 10)
+    this.password = hash
+  }
+
+  checkPassword(pass: string): Promise<boolean> {
+    return bcrypt.compare(pass, this.password)
+  }
+
+}
