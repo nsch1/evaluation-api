6d395b73a207fae23751eee3ffcd4d607124ee0e
nsch1 <nick.schooneman1@gmail.com>
2018-04-03 14:05:09 +0200

    Add test for logincontroller

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import app from "./app";
 
 const port = process.env.PORT || 4008
 
-setupDb()
+setupDb(true)
   .then(_ => {
     app.listen(port, () => console.log(`Listening on port ${port}`))
   })
