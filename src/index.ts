f100c0c8f40b9e2844ae51461e6f433662be4a6a
nsch1 <nick.schooneman1@gmail.com>
2018-04-03 13:27:39 +0200

    Add logincontroller in server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import 'reflect-metadata'
 import {createKoaServer} from "routing-controllers"
 import setupDb from './db'
+import LoginController from "./logins/controller";
 
 const port = process.env.PORT || 4008
 
 const app = createKoaServer({
   controllers: [
-    //..
+    LoginController
   ]
 })
 
