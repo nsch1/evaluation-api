dce33516cae62d18f78d07e504f219ab50d942d7
nsch1 <nick.schooneman1@gmail.com>
2018-04-03 21:06:59 +0200

    Add get route for all classes

--- src/groups/controller.ts
+++ src/groups/controller.ts
@@ -1,9 +1,14 @@
-import {Body, HttpCode, JsonController, Post} from "routing-controllers";
+import {Body, Get, HttpCode, JsonController, Post} from "routing-controllers";
 import Group from "./entity";
 
 @JsonController()
 export default class GroupController {
 
+  @Get('/classes')
+  getGroups() {
+    return Group.find()
+  }
+  
   @Post('/classes')
   @HttpCode(201)
   createGroup(
