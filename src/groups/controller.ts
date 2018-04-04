12d209644459ff2892b5e4b5bd947b2e2535aeab
nsch1 <nick.schooneman1@gmail.com>
2018-04-04 09:33:01 +0200

    Add get route for single class

--- src/groups/controller.ts
+++ src/groups/controller.ts
@@ -1,5 +1,8 @@
-import {Body, Get, HttpCode, JsonController, Post} from "routing-controllers";
+import {Body, Get, HttpCode, JsonController, NotFoundError, Param, Post} from "routing-controllers";
 import Group from "./entity";
+import {getRepository} from "typeorm";
+import Student from "../students/entity";
+import Evaluation from "../evaluations/entity";
 
 @JsonController()
 export default class GroupController {
@@ -8,6 +11,29 @@ export default class GroupController {
   async getGroups() {
     const classes = await Group.find()
     return {classes}
+  }
+
+  @Get('/classes/:id([0-9]+)')
+  async getGroup(
+    @Param('id') groupId: number
+  ) {
+    const group = await getRepository(Group)
+      .createQueryBuilder('g')
+      .where('g.id = :id', {id: groupId})
+      .leftJoinAndSelect('g.students', 's')
+      .leftJoinAndSelect('s.evaluations', 'e')
+      .getOne()
+
+    if(!group) throw new NotFoundError('No class found.')
+
+    group.students.forEach(s => {
+      s.evaluations.sort((a, b) => {
+        return Number(new Date(b.date)) - Number(new Date(a.date))
+      })
+    })
+
+    return group
+
   }
   
   @Post('/classes')
