efb343aae3c7fd469868f42af9207494e9621013
nsch1 <nick.schooneman1@gmail.com>
2018-04-05 18:32:57 +0200

    Add more changes to accomodate frontend my brain is fried

--- src/students/controller.ts
+++ src/students/controller.ts
@@ -1,4 +1,7 @@
-import {BadRequestError, Body, Get, HttpCode, JsonController, Param, Post} from "routing-controllers";
+import {
+  BadRequestError, Body, Delete, Get, HttpCode, JsonController, NotFoundError, Param, Patch,
+  Post
+} from "routing-controllers";
 import Student from "./entity";
 import Group from "../groups/entity";
 
@@ -11,20 +14,55 @@ export default class StudentController {
     @Body() body: Student,
     @Param('id') groupId: number
   ) {
-    const group = await Group.findOneById(groupId)
+    const group = await Group.findOne({where: {id: groupId}})
     if(!group) throw new BadRequestError("Class doesn't exist.")
 
-    return Student.create({
+    const student = await Student.create({
       ...body,
       group
     }).save()
+
+    return Student.findOne({where: {id: student.id}, relations: ['evaluations']})
   }
 
   @Get('/students/:id([0-9]+)')
-  getStudent(
+  async getStudent(
     @Param('id') studentId: number
   ) {
-    return Student.findOne({where: {id:studentId}, relations: ['evaluations']})
+    const student = await Student.findOne({where: {id:studentId}, relations: ['evaluations']})
+    if(!student) throw new NotFoundError('No student found.')
+
+    student.evaluations.sort((a, b) => {
+      return Number(new Date(b.date)) - Number(new Date(a.date))
+    })
+
+    return student
+  }
+
+  @Patch('/students/:id([0-9]+)')
+  async updateStudent(
+    @Body() update: Partial<Student>,
+    @Param('id') studentId: number
+  ) {
+    const student = await Student.findOne({where: {id: studentId}})
+    if(!student) throw new NotFoundError('No student found.')
+
+    if(update.name) student.name = update.name
+    if(update.picture) student.picture = update.picture
+
+    await student.save()
+
+    return student
+  }
+
+  @Delete('/students/:id([0-9]+)')
+  async deleteStudent(
+    @Param('id') studentId: number
+  ) {
+    const student = await Student.findOne({where: {id: studentId}})
+    if(!student) throw new NotFoundError('No student found.')
+
+    return student.remove()
   }
 
 }
